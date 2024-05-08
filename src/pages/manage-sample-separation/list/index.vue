e9f609d187c9abeb56283efdfb8da6eec3494ee0
Quangloi0727 <nguyenquangloi96@gmail.com>
2024-05-08 07:27:48 +0700

    update

---
+++ src/pages/manage-sample-separation/list/index.vue
@@ -0,0 +1,226 @@
+<script setup lang="ts">
+import type { UserProperties } from '@db/apps/users/types';
+
+// 👉 Store
+const searchQuery = ref('');
+const selectedStatus = ref();
+
+// Data table options
+const itemsPerPage = ref(10);
+const page = ref(1);
+const sortBy = ref();
+const orderBy = ref();
+
+// Update data table options
+const updateOptions = (options: any) => {
+    sortBy.value = options.sortBy[0]?.key;
+    orderBy.value = options.sortBy[0]?.order;
+};
+
+// Headers
+const headers = [
+    { title: 'Mã số hồ sơ', key: 'code', sortable: false },
+    { title: 'Giai đoạn', key: 'step', sortable: false },
+    { title: 'Người phụ trách', key: 'userCare', sortable: false },
+    { title: 'Tổng mẫu', key: 'totalTemplate', sortable: false },
+    { title: 'Ngày xử lý', key: 'processDate', sortable: false },
+    { title: 'Trạng thái', key: 'status', sortable: false },
+    { title: 'Actions', key: 'actions', sortable: false },
+];
+
+// 👉 Fetching users
+const { data: usersData, execute: fetchUsers } = await useApi<any>(createUrl('/apps/users', {
+    query: {
+        q: searchQuery,
+        status: selectedStatus,
+        itemsPerPage,
+        page,
+        sortBy,
+        orderBy,
+    },
+}));
+
+const users = computed((): UserProperties[] => usersData.value.users);
+const totalUsers = computed(() => usersData.value.totalUsers);
+
+const status = [
+    { title: 'Thành công', value: 'success' },
+    { title: 'Thất bại', value: 'fail' },
+];
+
+const resolveUserStatusVariant = (stat: string) => {
+    const statLowerCase = stat.toLowerCase();
+    if (statLowerCase === 'fail')
+        return 'warning';
+    if (statLowerCase === 'success')
+        return 'success';
+    return 'primary';
+};
+
+const getValueStatus = (st: string) => {
+    const findStatus = status.find(s => s.value === st);
+    if (findStatus) return findStatus.title;
+    return st;
+};
+
+// 👉 Add new user
+const addNewUser = async (userData: UserProperties) => {
+    await $api('/apps/users', {
+        method: 'POST',
+        body: userData,
+    });
+
+    // refetch User
+    fetchUsers();
+};
+
+// 👉 Delete user
+const deleteUser = async (id: number) => {
+    await $api(`/apps/users/${id}`, {
+        method: 'DELETE',
+    });
+
+    // refetch User
+    // TODO: Make this async
+    fetchUsers();
+};
+
+const widgetData = ref([
+    { value: '24', desc: 'Tổng hồ sơ', icon: 'tabler-users', iconColor: 'primary' },
+    { value: '165', desc: 'Hồ sơ thất bại', icon: 'tabler-address-book-off', iconColor: 'error' },
+    { value: '195', desc: 'Hồ sơ thành công', icon: 'tabler-checks', iconColor: 'success' }
+]);
+</script>
+
+<template>
+    <section>
+        <!-- 👉 Widgets -->
+        <div class="d-flex mb-6">
+            <VRow>
+                <template v-for="(data, id) in widgetData" :key="id">
+                    <VCol cols="12" md="4" sm="6">
+                        <VCard>
+                            <VCardText>
+                                <div class="d-flex justify-space-between">
+                                    <div class="d-flex flex-column gap-y-1">
+                                        <div class="d-flex gap-x-2 align-center">
+                                            <h4 class="text-h4">
+                                                {{ data.value }}
+                                            </h4>
+                                        </div>
+                                        <div class="text-sm">
+                                            {{ data.desc }}
+                                        </div>
+                                    </div>
+                                    <VAvatar :color="data.iconColor" variant="tonal" rounded size="42">
+                                        <VIcon :icon="data.icon" size="26" />
+                                    </VAvatar>
+                                </div>
+                            </VCardText>
+                        </VCard>
+                    </VCol>
+                </template>
+            </VRow>
+        </div>
+
+        <VCard class="mb-6">
+            <VDivider />
+
+            <VCardText class="d-flex flex-wrap gap-4">
+                <div class="me-3 d-flex gap-3">
+                    <AppSelect :model-value="itemsPerPage" :items="[
+                        { value: 10, title: '10' },
+                        { value: 25, title: '25' },
+                        { value: 50, title: '50' },
+                        { value: 100, title: '100' },
+                    ]" style="inline-size: 6.25rem;" @update:model-value="itemsPerPage = parseInt($event, 10)" />
+                    <!-- 👉 Add user button -->
+                    <VBtn prepend-icon="tabler-plus" :to="{ name: 'manage-sample-separation-new' }">
+                        Thêm hồ sơ
+                    </VBtn>
+                </div>
+                <VSpacer />
+
+                <div class="app-user-search-filter d-flex align-center flex-wrap gap-4">
+                    <!-- 👉 Search  -->
+                    <div style="inline-size: 10.625rem;">
+                        <!-- 👉 Select Plan -->
+                        <AppSelect v-model="selectedStatus" placeholder="Trạng thái" :items="status" clearable
+                            clear-icon="tabler-x" />
+                    </div>
+                </div>
+            </VCardText>
+
+            <VDivider />
+
+            <!-- SECTION datatable -->
+            <VDataTableServer v-model:items-per-page="itemsPerPage" v-model:page="page" :items="users"
+                :items-length="totalUsers" :headers="headers" class="text-no-wrap" show-select
+                @update:options="updateOptions">
+                <!-- User -->
+                <template #item.code="{ item }">
+                    <div class="d-flex flex-column">
+                        <h6 class="text-base">
+                            <RouterLink :to="{ name: 'apps-user-view-id', params: { id: item.id } }"
+                                class="font-weight-medium text-primary ">
+                                #{{ item.code }}
+                            </RouterLink>
+                        </h6>
+                    </div>
+                </template>
+                <!-- Status -->
+                <template #item.status="{ item }">
+                    <VChip :color="resolveUserStatusVariant(item.status)" size="small" label class="text-capitalize">
+                        {{ getValueStatus(item.status) }}
+                    </VChip>
+                </template>
+
+                <!-- Actions -->
+                <template #item.actions="{ item }">
+                    <IconBtn @click="deleteUser(item.id)">
+                        <VIcon icon="tabler-trash" />
+                    </IconBtn>
+
+                    <IconBtn>
+                        <VIcon icon="tabler-eye" />
+                    </IconBtn>
+
+                    <VBtn icon variant="text" color="medium-emphasis">
+                        <VIcon icon="tabler-dots-vertical" />
+                        <VMenu activator="parent">
+                            <VList>
+                                <VListItem :to="{ name: 'apps-user-view-id', params: { id: item.id } }">
+                                    <template #prepend>
+                                        <VIcon icon="tabler-eye" />
+                                    </template>
+
+                                    <VListItemTitle>View</VListItemTitle>
+                                </VListItem>
+
+                                <VListItem link>
+                                    <template #prepend>
+                                        <VIcon icon="tabler-pencil" />
+                                    </template>
+                                    <VListItemTitle>Edit</VListItemTitle>
+                                </VListItem>
+
+                                <VListItem @click="deleteUser(item.id)">
+                                    <template #prepend>
+                                        <VIcon icon="tabler-trash" />
+                                    </template>
+                                    <VListItemTitle>Delete</VListItemTitle>
+                                </VListItem>
+                            </VList>
+                        </VMenu>
+                    </VBtn>
+                </template>
+
+                <!-- pagination -->
+                <template #bottom>
+                    <TablePagination v-model:page="page" :items-per-page="itemsPerPage" :total-items="totalUsers" />
+                </template>
+            </VDataTableServer>
+            <!-- SECTION -->
+        </VCard>
+    </section>
+</template>
---
+++ src/pages/manage-sample-separation/new/index.vue
@@ -0,0 +1,158 @@
+<script setup lang="ts">
+import type { CustomInputContent } from '@core/types';
+
+const radioContent: CustomInputContent[] = [
+    {
+        title: 'Standard',
+        desc: 'Delivery in 3-5 days.',
+        value: 'standard',
+        icon: { icon: 'tabler-briefcase-2', size: '32' },
+    },
+    {
+        title: 'Express',
+        desc: 'Delivery within 2 days.',
+        value: 'express',
+        icon: { icon: 'tabler-rocket', size: '32' },
+    },
+    {
+        title: 'Overnight',
+        desc: 'Delivery within a days.',
+        value: 'overnight',
+        icon: { icon: 'tabler-crown', size: '32' },
+    },
+];
+
+const promoCodeList = [
+    {
+        code: 'TAKEITALL',
+        desc: 'Apply this code to get 15% discount on orders above 20$.',
+    },
+    {
+        code: 'FESTIVE10',
+        desc: 'Apply this code to get 10% discount on all orders.',
+    },
+    {
+        code: 'MYSTERYDEAL',
+        desc: 'Apply this code to get discount between 10% - 50%.',
+    },
+];
+
+const formData = ref({
+    fullName: '',
+    email: '',
+    contactNumber: null,
+    altContactNumber: null,
+    address: '',
+    pincode: null,
+    Landmark: '',
+    city: '',
+    state: null,
+    defaultAddress: false,
+    addressType: 'home',
+    deliveryType: 'overnight',
+    promoCode: '',
+    paymentMethod: 'card',
+    cardNumber: null,
+    cardName: '',
+    cardExDate: '',
+    cardCvv: '',
+});
+</script>
+
+<template>
+    <VCard class="overflow-visible">
+        <div class="w-100 sticky-header overflow-hidden rounded-t">
+            <div class=" d-flex align-center gap-4 flex-wrap bg-custom-background pa-2">
+                <VCardTitle>Thêm mới hồ sơ</VCardTitle>
+                <VSpacer />
+                <div>
+                    <VBtn variant="tonal" class="me-4">
+                        Hủy
+                    </VBtn>
+                    <VBtn>Lưu</VBtn>
+                </div>
+            </div>
+        </div>
+
+        <VCardText>
+            <VRow>
+                <VCol md="11" cols="12" class="mx-auto">
+                    <VForm>
+                        <h5 class="text-h6 mb-6">
+                            Thông tin hồ sơ
+                        </h5>
+                        <VRow>
+                            <VCol cols="12" md="12">
+                                <AppTextField v-model="formData.fullName" label="Tên hồ sơ" placeholder="Nhập..." />
+                            </VCol>
+
+                            <VCol cols="12" md="6">
+                                <AppSelect v-model="formData.state" label="Người phụ trách" placeholder="--- Chọn ---"
+                                    :items="['Nguyễn văn A', 'Nguyễn văn B', 'Nguyễn văn C']" clear-icon="tabler-x"
+                                    clearable />
+                            </VCol>
+
+                            <VCol cols="12" md="6">
+                                <AppDateTimePicker v-model="formData.contactNumber" label="Ngày tạo"
+                                    prepend-inner-icon="tabler-calendar" placeholder="Chọn ngày tạo"
+                                    :config="{ dateFormat: 'd/m/Y' }" />
+                            </VCol>
+
+                            <VCol cols="12">
+                                <div>
+                                    <TiptapEditor v-model="formData.contactNumber" label="Ghi chú"
+                                        class="border rounded basic-editor" />
+                                </div>
+                            </VCol>
+                        </VRow>
+                    </VForm>
+                </VCol>
+            </VRow>
+        </VCardText>
+    </VCard>
+</template>
+
+<style lang="scss" scoped>
+.sticky-header {
+    position: sticky;
+    z-index: 9;
+    transition: all 0.3s ease-in-out;
+}
+
+.layout-nav-type-vertical {
+    &.layout-navbar-sticky {
+        .sticky-header {
+            inset-block: 4.3rem 0;
+        }
+    }
+
+    &.layout-navbar-static {
+        .sticky-header {
+            inset-block: 0 0;
+        }
+    }
+}
+
+.layout-nav-type-horizontal {
+    &.layout-navbar-static {
+        .sticky-header {
+            inset-block: 0 0;
+        }
+    }
+
+    &.layout-navbar-sticky {
+        .sticky-header {
+            inset-block: 6.75rem 0;
+        }
+    }
+}
+
+.basic-editor {
+    .ProseMirror {
+        block-size: 200px;
+        outline: none;
+        overflow-y: auto;
+        padding-inline: 0.5rem;
+    }
+}
+</style>
