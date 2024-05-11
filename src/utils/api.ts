9f1129d58406487af6ae057e312c097935d2e1b9
Quangloi0727 <nguyenquangloi96@gmail.com>
2024-05-12 03:32:28 +0700

    done dau vet hinh su

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,14 +1,40 @@
-import { ofetch } from 'ofetch'
+import { ofetch } from 'ofetch';
 
 export const $api = ofetch.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   async onRequest({ options }) {
-    const accessToken = useCookie('accessToken').value
+    const accessToken = useCookie('accessToken').value;
     if (accessToken) {
       options.headers = {
         ...options.headers,
         Authorization: `Bearer ${accessToken}`,
-      }
+      };
     }
   },
-})
+});
+
+export const $fetchApiImageService = ofetch.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL_CREATE_IMAGE || '/api',
+  async onRequest({ options }) {
+    const accessToken = useCookie('accessToken').value;
+    if (accessToken) {
+      options.headers = {
+        ...options.headers,
+        Authorization: `Bearer ${accessToken}`,
+      };
+    }
+  },
+});
+
+export const $fetchApiAiService = ofetch.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL_AI || '/api',
+  async onRequest({ options }) {
+    const accessToken = useCookie('accessToken').value;
+    if (accessToken) {
+      options.headers = {
+        ...options.headers,
+        Authorization: `Bearer ${accessToken}`,
+      };
+    }
+  },
+});
