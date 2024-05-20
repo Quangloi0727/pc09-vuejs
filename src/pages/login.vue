5101449619667e2d199fba9302351beac61f103a
Quangloi0727 <nguyenquangloi96@gmail.com>
2024-05-20 18:43:36 +0700

    fix some bug

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,19 +1,9 @@
-<!-- ❗Errors in the form are set on line 60 -->
 <script setup lang="ts">
-import { useGenerateImageVariant } from '@core/composable/useGenerateImageVariant';
-import authV2LoginIllustrationBorderedDark from '@images/pages/auth-v2-login-illustration-bordered-dark.png';
-import authV2LoginIllustrationBorderedLight from '@images/pages/auth-v2-login-illustration-bordered-light.png';
-import authV2LoginIllustrationDark from '@images/pages/auth-v2-login-illustration-dark.png';
-import authV2LoginIllustrationLight from '@images/pages/auth-v2-login-illustration-light.png';
-import authV2MaskDark from '@images/pages/misc-mask-dark.png';
-import authV2MaskLight from '@images/pages/misc-mask-light.png';
+import authV1BottomShape from '@images/svg/auth-v1-bottom-shape.svg?raw';
+import authV1TopShape from '@images/svg/auth-v1-top-shape.svg?raw';
 import { VNodeRenderer } from '@layouts/components/VNodeRenderer';
 import { themeConfig } from '@themeConfig';
 import { VForm } from 'vuetify/components/VForm';
-
-const authThemeImg = useGenerateImageVariant(authV2LoginIllustrationLight, authV2LoginIllustrationDark, authV2LoginIllustrationBorderedLight, authV2LoginIllustrationBorderedDark, true);
-
-const authThemeMask = useGenerateImageVariant(authV2MaskLight, authV2MaskDark);
 
 definePage({
   meta: {
@@ -84,71 +74,70 @@ const onSubmit = () => {
 </script>
 
 <template>
-  <RouterLink to="/">
-    <div class="auth-logo d-flex align-center gap-x-3">
-      <VNodeRenderer :nodes="themeConfig.app.logo" />
-      <h1 class="auth-title">
-        {{ themeConfig.app.title }}
-      </h1>
-    </div>
-  </RouterLink>
+  <div class="auth-wrapper d-flex align-center justify-center pa-4">
+    <div class="position-relative my-sm-16">
+      <!-- 👉 Top shape -->
+      <VNodeRenderer :nodes="h('div', { innerHTML: authV1TopShape })"
+        class="text-primary auth-v1-top-shape d-none d-sm-block" />
 
-  <VRow no-gutters class="auth-wrapper bg-surface">
-    <VCol md="8" class="d-none d-md-flex">
-      <div class="position-relative bg-background w-100 me-0">
-        <div class="d-flex align-center justify-center w-100 h-100" style="padding-inline: 6.25rem;">
-          <VImg max-width="613" :src="authThemeImg" class="auth-illustration mt-16 mb-2" />
-        </div>
+      <!-- 👉 Bottom shape -->
+      <VNodeRenderer :nodes="h('div', { innerHTML: authV1BottomShape })"
+        class="text-primary auth-v1-bottom-shape d-none d-sm-block" />
+      <!-- 👉 Auth Card -->
+      <VCard class="auth-card" max-width="460" :class="$vuetify.display.smAndUp ? 'pa-6' : 'pa-0'">
+        <VCardItem class="justify-center">
+          <VCardTitle>
+            <div class="app-logo">
+              <VNodeRenderer :nodes="themeConfig.app.logo" />
+              <h1 class="app-logo-title">
+                {{ themeConfig.app.title }}
+              </h1>
+            </div>
+          </VCardTitle>
+        </VCardItem>
 
-        <img class="auth-footer-mask" :src="authThemeMask" alt="auth-footer-mask" height="280" width="100">
-      </div>
-    </VCol>
-
-    <VCol cols="12" md="4" class="auth-card-v2 d-flex align-center justify-center">
-      <VCard flat :max-width="500" class="mt-sm-0 pa-4">
         <VCardText>
           <h4 class="text-h4 mb-1">
-            Welcome to <span class="text-capitalize"> {{ themeConfig.app.title }} </span>! 👋🏻
+            Welcome to <span class="text-capitalize">{{ themeConfig.app.title }}</span>! 👋🏻
           </h4>
-          <p class="mb-0">
-            PC09_HaNoi
-          </p>
         </VCardText>
+
         <VCardText>
           <VForm ref="refVForm" @submit.prevent="onSubmit">
             <VRow>
               <!-- email -->
               <VCol cols="12">
-                <AppTextField v-model="credentials.email" label="Số điện thoại" placeholder="johndoe@email.com"
-                  type="text" autofocus :rules="[requiredValidator, emailValidator]" :error-messages="errors.email" />
+                <AppTextField v-model="credentials.email" autofocus label="Email or Username" type="email"
+                  placeholder="johndoe@email.com" />
               </VCol>
 
               <!-- password -->
               <VCol cols="12">
-                <AppTextField v-model="credentials.password" label="Mật khẩu" placeholder="············"
-                  :rules="[requiredValidator]" :type="isPasswordVisible ? 'text' : 'password'"
-                  :error-messages="errors.password"
+                <AppTextField v-model="credentials.password" label="Password" placeholder="············"
+                  :type="isPasswordVisible ? 'text' : 'password'"
                   :append-inner-icon="isPasswordVisible ? 'tabler-eye-off' : 'tabler-eye'"
                   @click:append-inner="isPasswordVisible = !isPasswordVisible" />
-
+                <!-- login button -->
                 <div class="d-flex align-center flex-wrap justify-space-between my-6">
                   <VCheckbox v-model="rememberMe" label="Remember me" />
                   <RouterLink class="text-primary ms-2 mb-1" :to="{ name: 'forgot-password' }">
                     Forgot Password?
                   </RouterLink>
                 </div>
-
                 <VBtn block type="submit">
                   Login
                 </VBtn>
               </VCol>
               <!-- auth providers -->
+              <VCol cols="12" class="text-center">
+                <AuthProvider />
+              </VCol>
             </VRow>
           </VForm>
         </VCardText>
       </VCard>
-    </VCol>
-  </VRow>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
